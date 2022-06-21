6053e8f81fa24cde0a33ac7c7dbaf31d59f92f13
JimcostDev <rjak727@gmail.com>
2022-06-21 19:14:19 +0200

    practica con ejercicio para contar caracteres

---
+++ ejecicios_logica_programacion/contar_caracteres.js
@@ -0,0 +1,7 @@
+function contar_caracteres(cadena){
+    total_caracteres = cadena.length
+    return total_caracteres
+};
+
+//str = prompt('Hola, como te llamas: ');
+console.log(` la cadena tiene ${contar_caracteres('Ronaldo')} caracteres`);
