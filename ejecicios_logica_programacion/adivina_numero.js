adbec4841c02557583a9394d15a9ad88f430314d
JimcostDev <rjak727@gmail.com>
2022-06-22 18:22:39 +0200

    algoritmo para adivinar numero

---
+++ ejecicios_logica_programacion/adivina_numero.js
@@ -0,0 +1,24 @@
+// tines 3 intentos para adivinar un número aleatorio del 0 al 10, suerte.
+function getRandom() {
+    let num = Math.random()*10;
+    return parseInt(num);
+}
+
+// contador de intentos
+let intentos = 0;
+let numero = getRandom();
+
+while(true){
+    let adivina = parseInt(prompt('Escribe un número, solo tienes 3 intentos: '));
+    intentos += 1
+    if(intentos == 3){
+        alert(` perdiste, el número era: ${numero}`);
+        break;
+    }
+    else if (adivina == numero){
+        alert('Felecidades, adivinaste')
+        break;
+    }
+}
+
+console.log(numero);
