0954348ea04f84e4f678f0c9e5cd10ebdc752d77
JimcostDev <rjak727@gmail.com>
2022-08-24 18:56:41 +0200

    explication slice and index of

--- ejecicios_logica_programacion/hasDuplicates.js
+++ ejecicios_logica_programacion/hasDuplicates.js
@@ -23,3 +23,14 @@ try {
 } catch (error) {
     console.log(`Error: ${error.message}`);
 }
+
+
+// EXPLICATION
+console.log('EXPLICATION: ');
+//         idx = 0, 1, 2, 3, 4, 5
+const numbers = [1, 2, 3, 8, 1, 4];
+console.log(numbers);
+let numbers_slice = numbers.slice(1); // next idx
+console.log(numbers_slice);
+const result = numbers_slice.indexOf(numbers[0]) //find the current item, returns -1 if not found
+console.log(result);
