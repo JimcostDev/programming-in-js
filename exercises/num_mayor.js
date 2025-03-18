2e4c64f640c8679e0dd0fa81f01a72c396f1c072
JimcostDev <rjak727@gmail.com>
2025-03-18 21:48:17 +0100

    obtener número mayor, otra forma

--- exercises/num_mayor.js
+++ exercises/num_mayor.js
@@ -10,6 +10,24 @@ function findLargestNumber(num1, num2, num3) {
     return num3;
   }
 }
+
+// forma dinamica de hacerlo, leer varios números
+function readNumber(key) {
+    let numeroUsuario = parseInt(prompt(`Ingrese el ${key} número: `));
+    return numeroUsuario;
+}
+
+// encontrar el número mayor
+function numeroMayor(numeros) {
+    let mayor = numeros[0];
+    for (let x of numeros) {
+        if (x > mayor) {
+            mayor = x;
+        }
+    }
+    return mayor;
+}
+
 // función para correr el programa
 function run(){
     const num1 = parseInt(prompt("Ingrese el primer número: "));
@@ -21,6 +39,17 @@ function run(){
     // otra forma de hacerlo con la función max
     const mayor2 = Math.max(num1, num2, num3);
     console.log(`Usando la función max: ${mayor2}`);
+
+    // forma dinámica
+    console.log("Forma dinámica");5
+    let primerNumero = readNumber("primer");
+    let segundoNumero = readNumber("segundo");
+    let tercerNumero = readNumber("tercer");
+    let cuartoNumero = readNumber("cuarto");
+    let quintoNumero = readNumber("quinto");
+    let numeros = [primerNumero, segundoNumero, tercerNumero, cuartoNumero, quintoNumero];
+    let resultado = numeroMayor(numeros);
+    console.log(`El número mayor es (forma dinámica): ${resultado}`);
 }
 
 run();
