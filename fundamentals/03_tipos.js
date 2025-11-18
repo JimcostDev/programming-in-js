24f7410315b4fe910cf625b24ea1a31f45a71bb0
JimcostDev <rjak727@gmail.com>
2025-11-18 13:14:24 +0100

    tipos de datos

--- fundamentals/03_tipos.js
+++ fundamentals/03_tipos.js
@@ -1,23 +1,23 @@
 // Algoritmo que muestra el tipo de dato de una variable.
 
 // Variable para almacenar diferentes tipos de datos
-let variable = 8;
+let variable = [1, 2, 3]; 
 let tipoDato = typeof variable;
 
 if (tipoDato === 'number') {
   if (Number.isInteger(variable)) {
-    console.log('La variable es un número de tipo: entero');
+    console.log(`La variable es un número de tipo: entero (${tipoDato}) `);
   } else {
-    console.log('La variable es un número de tipo: float');
+    console.log(`La variable es un número de tipo: float (${tipoDato})`);
   }
 } else if (tipoDato === 'string') {
-  console.log('La variable es de tipo: cadena');
+  console.log(`La variable es de tipo: cadena (${tipoDato})`);
 } else if (tipoDato === 'boolean') {
-  console.log('La variable es de tipo: booleana');
+  console.log(`La variable es de tipo: booleana (${tipoDato})`);
 } else if (Array.isArray(variable)) {
-  console.log('La variable es de tipo: lista');
+  console.log(`La variable es de tipo: lista (${tipoDato})`);
 } else if (tipoDato === 'object') {
-  console.log('La variable es de tipo: diccionario');
+  console.log(`La variable es de tipo: diccionario (${tipoDato})`);
 } else {
   console.log('Tipo de dato no reconocido');
 }
