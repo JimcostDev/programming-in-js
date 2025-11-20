8d911d87cbb0d47d8d4f35ed85631522ed5ed8fc
JimcostDev <rjak727@gmail.com>
2025-11-20 15:55:41 +0100

    mejorando ejemplo de metodos en arreglos

--- data_structure/metodos_arreglos.js
+++ data_structure/metodos_arreglos.js
@@ -1,38 +1,44 @@
 // CRUD: Create, Read, Update & Delete en arreglos
 
 // Crear y leer elementos
-let numeros = [1, 2, 3, 4, 5];
-console.log(numeros[1]); // Imprime el segundo elemento (2)
+let teams = ['colombia', 'argentina', 'brasil'];
+console.log(teams);
+console.log(teams[1]); 
 
-// Actualizar el último elemento (equivalente a numeros[-1] en Python)
-numeros[numeros.length - 1] = 10;
-console.log(numeros); // [1, 2, 3, 4, 10]
+// Actualizar el último elemento (equivalente a teams[-1] en Python)
+let ultimo = teams.length - 1;
+teams[ultimo] = 'uruguay';
+console.log(teams); 
 
 // Agregar valores al final usando push (equivalente a append)
-numeros.push(700);
-console.log(numeros); // [1, 2, 3, 4, 10, 700]
+teams.push('españa');
+console.log(teams); 
+
+// Agregar valores al inicio usando unshift
+teams.unshift('portugal');
+console.log(teams);
 
 // Insertar un valor en un índice específico (equivalente a insert)
-numeros.splice(3, 0, 'hola'); // Inserta 'hola' en el índice 3
-console.log(numeros);
+teams.splice(2, 0, 'francia'); // en el índice 2, eliminar 0 elementos, agregar 'francia'
+console.log(teams);
 
 // Concatenar o unir arreglos
-let tareas = ['tarea 1', 'tarea 2', 'tarea 3'];
-let nueva_lista = numeros.concat(tareas);
+let players = ['james', 'ronaldo', 'messi'];
+let nueva_lista = teams.concat(players);
 console.log(nueva_lista);
 
 // Buscar el índice de un elemento (equivalente a index)
-let indice = nueva_lista.indexOf('tarea 3');
+let indice = nueva_lista.indexOf('messi');
 console.log(indice);
 
 // Actualizar el elemento encontrado
-nueva_lista[indice] = 'tarea nueva 3';
+nueva_lista[indice] = 'haaland';
 console.log(nueva_lista);
 
 // Eliminar valores
 // Remover por valor: encontrar el índice y usar splice
-let idx = nueva_lista.indexOf('tarea 1');
-if (idx !== -1) {
+let idx = nueva_lista.indexOf('francia');
+if (idx !== -1) { // -1 significa que el elemento fue encontrado
   nueva_lista.splice(idx, 1);
 }
 console.log(nueva_lista);
@@ -50,12 +56,18 @@ nueva_lista.reverse();
 console.log(nueva_lista);
 
 // Ordenar arreglos
-let numeros_a = [1, 4, 6, 3];
+let numeros = [1, 4, 6, 3];
 // Para números, se debe pasar una función de comparación
-numeros_a.sort((a, b) => a - b);
-console.log(numeros_a);
+numeros.sort((a, b) => a - b);
+console.log(numeros);
 
 let strings = ['re', 'ab', 'ed'];
 strings.sort(); // Ordena alfabéticamente
 console.log(strings);
+
+// forEach para iterar sobre los elementos
+let frutas = ['manzana', 'banana', 'cereza'];
+frutas.forEach((fruta, indice, arreglo) => {
+  console.log(`Fruta: ${fruta}, Índice: ${indice}, Arreglo: ${arreglo.join(', ')}`);
+});
 
